fed6923655d018af5005d3ffbb335a46d2831806
Benjamín <144442997+BenjaminMasopust@users.noreply.github.com>
2023-09-27 21:06:28 +0200

    přidání zák. souborů

---
+++ app.js
@@ -0,0 +1,55 @@
+let input = document.querySelector('.entered-list');
+let addBtn = document.querySelector('.add-list');
+let tasks = document.querySelector('.tasks');
+
+//aktivace a deaktivace tlačítka add
+
+input.addEventListener('keyup', () => {
+    if(input.value.trim() != 0){
+        addBtn.classList.add('active')
+    }else {
+        addBtn.classList.remove('active')
+
+
+    }
+})
+
+//přidání nového úkolu
+
+addBtn.addEventListener('click', () => {
+    if (input.value.trim() != 0) {
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = ` 
+        <p> ${input.value} </p>
+        <div class="item-btn">   
+            <i class="fa-regular fa-pen-to-square"></i>
+            <i class="fa-sharp fa-solid fa-xmark"></i>
+        </div>
+        `
+        
+        tasks.appendChild(newItem);
+        input.value = '';
+    } else{
+        alert('prosím zadejte úkol')
+    }
+
+})
+
+
+//smazání úkolu ze seznamu
+
+    tasks.addEventListener('click', (e) => {
+        if (e.target.classList.contains('fa-xmark')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    })
+
+//označit úkol jako splněný
+
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-pen-to-square')
+    ) {
+        e.target.parentElement.parentElement.classList.toggle('completed');
+    }
+})
